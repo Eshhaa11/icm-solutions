b5b4d659a9a5d783e0526820c963f5e59ed70e8e
Esha Patel <Patellesha11@gmail.com>
2025-05-05 22:10:07 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <NavBar />
       <IntroComponent />
-      <ContactUs />
       <AboutUs />
+      <ContactUs />
       <Faq />
       <Terms/>
 
